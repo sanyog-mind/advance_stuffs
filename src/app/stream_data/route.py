40f348a6d2fe523138dc15e0a85af4f2de8342d2
Tirth Shah <tirth.shah@300minds.com>
2024-12-18 10:35:07 +0530

    final

--- src/app/stream_data/route.py
+++ src/app/stream_data/route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
-from src.app.stream_data.service import fetch_news, stream_to_kafka, consume_from_kafka, save_to_csv
+from src.app.stream_data.service import fetch_news, save_to_csv
 
 stream_router = APIRouter()
 
